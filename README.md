7cfcc9802f8b4ea3e25fb916d163c1ba38d58ff6
Nolan Bjarne <derjuniorschiemann@gmail.com>
2024-07-04 21:56:48 +0200

    Initialize new vue project which utilizes SPA development

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# immoflow-task
+# immoflow_test_task
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="vue-testing__register-form container">
+    <form class="form" @submit.prevent="handleSubmit">
+      <h3>Anmelden</h3>
+      <div class="form-group mb-2">
+        <input class="form-control" v-model="formData.email" name="email" type="email" placeholder="Ihre E-Mail-Adresse ..." />
+      </div>
+      <div class="form-group mb-2">
+        <input class="form-control" v-model="formData.password" name="password" type="password" placeholder="Ihr Passwort ..." />
+      </div>
+      <button class="btn btn-primary" type="submit">Anmelden</button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const formData = reactive({
+  email: '',
+  password: ''
+});
+
+const handleSubmit = () => {
+  // Handle form submission here, e.g., validation, API call
+  console.log('Form submitted with:', formData);
+};
+</script>
